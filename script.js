c8255a62bf8af907f24ae69ff7585ad5e6c900f1
graci <136918669+gracimarch@users.noreply.github.com>
2024-08-18 21:31:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -106,6 +106,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
             const targetId = this.getAttribute('href');
             const targetElement = document.querySelector(targetId);
