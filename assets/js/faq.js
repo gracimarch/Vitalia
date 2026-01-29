64ec97d535f66f8e8b8962e05686ab0cb9c08567
Graciana <gracianamarch1@gmail.com>
2026-01-29 19:10:58 -0300

    correcciones en la lógica del formulario + unificación de archivos

---
+++ assets/js/faq.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.question').forEach(question => {
+  question.addEventListener('click', () => {
+      question.classList.toggle('active');
+      
+      const answer = question.nextElementSibling;
+      if (question.classList.contains('active')) {
+          answer.style.maxHeight = answer.scrollHeight + 'px';
+      } else {
+          answer.style.maxHeight = '0';
+      }
+  });
+});
