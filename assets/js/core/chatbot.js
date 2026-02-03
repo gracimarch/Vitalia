7090987ded7fb352af689cdd37063e40b31091af
Graciana <gracianamarch1@gmail.com>
2026-02-03 20:56:03 -0300

    chatbot integrado

---
+++ assets/js/core/chatbot.js
@@ -0,0 +1,8 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'bcefa5f207b5ed0656433fb0ced1b567b133551f';
+window.smartsupp || (function (d) {
+    var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
+    s = d.getElementsByTagName('script')[0]; c = d.createElement('script');
+    c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
+    c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
+})(document);
